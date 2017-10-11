064a7d653c9f3634e7abf0d702f6bd6809da6e1b
Tristan Boulanger <contact@tristanboulanger.fr>
2017-10-11 22:57:50 +0200

    Webpack Config test2

--- src/App.js
+++ src/App.js
@@ -4,16 +4,16 @@ import ReactDOM from 'react-dom'
 import Header from './components/Header.js'
 
 class App extends React.Component {
-	render() {
-		return (
-			<div>
-				<Header />
-			</div>
-		)
-	}
+    render() {
+        return (
+            <div>
+                <Header />
+            </div>
+        )
+    }
 }
 
 ReactDOM.render(
-	<App/>,
-	document.getElementById('root')
+    <App/>,
+    document.getElementById('root')
 )
--- webpack.config.js
+++ webpack.config.js
@@ -4,67 +4,67 @@ const dev                   = process.env.NODE_ENV === "dev"
 const ExtractTextPlugin     = require("extract-text-webpack-plugin")
 
 let cssLoaders = [
-	{ loader: 'css-loader', options: { importLoaders: 1, minimize: !dev } }
+    { loader: 'css-loader', options: { importLoaders: 1, minimize: !dev } }
 ]
 if(!dev) {
-	cssLoaders.push({
-		loader: 'postcss-loader',
-		options: {
-			plugins: (loader) => [
-				require('autoprefixer')()
-			]
-		}
-	})
+    cssLoaders.push({
+        loader: 'postcss-loader',
+        options: {
+            plugins: (loader) => [
+                require('autoprefixer')()
+            ]
+        }
+    })
 }
 
 let config = {
-	entry: {
-		app: './src/App.js'
-	},
-	watch: dev,
-	output: {
-		filename: '[name].js',
-		path: path.resolve(__dirname, './dist')
-	},
-	devtool: dev ? 'cheap-module-eval-source-map' : false,
-	module: {
-		rules: [
-			{
-				test: /\.js$/,
-				exclude: /(node_modules)/,
-				use: ['babel-loader']
-			},
-			{
-				test: /\.css$/,
-				use: ExtractTextPlugin.extract({
-					fallback: 'style-loader',
-					use: cssLoaders
-				})
-			},
-			{
-				test: /\.scss$/,
-				use: ExtractTextPlugin.extract({
-					fallback: 'style-loader',
-					use: [
-						...cssLoaders,
-						'sass-loader'
-					]
-				})
-			}
-		]
-	},
-	plugins: [
-		new ExtractTextPlugin({
-			filename: '[name].css',
-			disable: dev
-		}),
-	]
+    entry: {
+        app: './src/App.js'
+    },
+    watch: dev,
+    output: {
+        filename: '[name].js',
+        path: path.resolve(__dirname, './dist')
+    },
+    devtool: dev ? 'cheap-module-eval-source-map' : false,
+    module: {
+        rules: [
+            {
+                test: /\.js$/,
+                exclude: /(node_modules)/,
+                use: ['babel-loader']
+            },
+            {
+                test: /\.css$/,
+                use: ExtractTextPlugin.extract({
+                    fallback: 'style-loader',
+                    use: cssLoaders
+                })
+            },
+            {
+                test: /\.scss$/,
+                use: ExtractTextPlugin.extract({
+                    fallback: 'style-loader',
+                    use: [
+                        ...cssLoaders,
+                        'sass-loader'
+                    ]
+                })
+            }
+        ]
+    },
+    plugins: [
+        new ExtractTextPlugin({
+            filename: '[name].css',
+            disable: dev
+        }),
+    ]
 }
 
 if(!dev) {
-	config.plugins.push(new UglifyJSPlugin({
-		sourceMap: false
-	}))
+    config.plugins.push(new UglifyJSPlugin({
+        sourceMap: false
+    }))
 }
 
 module.exports = config
