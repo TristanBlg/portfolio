c3702949b5cbfdf849a6c5b37f907f49381cde0f
Tristan Boulanger <contact@tristanboulanger.fr>
2017-10-11 22:42:26 +0200

    Webpack Config ~bug

--- webpack.config.js
+++ webpack.config.js
@@ -1,20 +1,20 @@
-const path 								= require('path')
-const UglifyJSPlugin 			= require('uglifyjs-webpack-plugin')
-const dev 								= process.env.NODE_ENV === "dev"
-const ExtractTextPlugin 	= require("extract-text-webpack-plugin")
+const path                  = require('path')
+const UglifyJSPlugin        = require('uglifyjs-webpack-plugin')
+const dev                   = process.env.NODE_ENV === "dev"
+const ExtractTextPlugin     = require("extract-text-webpack-plugin")
 
 let cssLoaders = [
-  { loader: 'css-loader', options: { importLoaders: 1, minimize: !dev } }
+    { loader: 'css-loader', options: { importLoaders: 1, minimize: !dev } }
 ]
 if(!dev) {
-	cssLoaders.push({
-		loader: 'postcss-loader',
-		options: {
-			plugins: (loader) => [
-				require('autoprefixer')()
-			]
+    cssLoaders.push({
+        loader: 'postcss-loader',
+        options: {
+            plugins: (loader) => [
+                require('autoprefixer')()
+            ]
 		}
-	})
+    })
 }
 
 let config = {
