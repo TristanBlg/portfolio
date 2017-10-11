4320bebd14dc530bee102d9fb467f05c45fc646f
Tristan Boulanger <contact@tristanboulanger.fr>
2017-10-11 23:20:18 +0200

    Webpack ~bug

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 
-import Header from './components/Header.js'
+import Header from './components/Header'
 
 class App extends React.Component {
     render() {
