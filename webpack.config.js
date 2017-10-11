fdeb0bd8af1b0e4425103aede79525aef87902bc
Tristan Boulanger <contact@tristanboulanger.fr>
2017-10-11 22:39:58 +0200

    Webpack Config ~bug

--- webpack.config.js
+++ webpack.config.js
@@ -1,20 +1,20 @@
-const path 					= require('path')
-const UglifyJSPlugin 		= require('uglifyjs-webpack-plugin')
-const dev 					= process.env.NODE_ENV === "dev"
+const path 								= require('path')
+const UglifyJSPlugin 			= require('uglifyjs-webpack-plugin')
+const dev 								= process.env.NODE_ENV === "dev"
 const ExtractTextPlugin 	= require("extract-text-webpack-plugin")
 
 let cssLoaders = [
-  	{ loader: 'css-loader', options: { importLoaders: 1, minimize: !dev } }
+  { loader: 'css-loader', options: { importLoaders: 1, minimize: !dev } }
 ]
 if(!dev) {
 	cssLoaders.push({
-  		loader: 'postcss-loader',
-  		options: {
-  			plugins: (loader) => [
-  				require('autoprefixer')()
-  			]
-  		}
-  	})
+		loader: 'postcss-loader',
+		options: {
+			plugins: (loader) => [
+				require('autoprefixer')()
+			]
+		}
+	})
 }
 
 let config = {
@@ -35,22 +35,22 @@ let config = {
 				use: ['babel-loader']
 			},
 			{
-		        test: /\.css$/,
-	            use: ExtractTextPlugin.extract({
+	      test: /\.css$/,
+        use: ExtractTextPlugin.extract({
 					fallback: 'style-loader',
 					use: cssLoaders
-			    })
-		    },
+		    })
+	    },
 			{
-		        test: /\.scss$/,
-	            use: ExtractTextPlugin.extract({
+	      test: /\.scss$/,
+        use: ExtractTextPlugin.extract({
 					fallback: 'style-loader',
 					use: [
 						...cssLoaders,
 						'sass-loader'
 					]
-			    })
-		    }
+		    })
+	    }
 		]
 	},
 	plugins: [
