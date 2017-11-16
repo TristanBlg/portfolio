d0047fbac5b39496aaf368168e8b4b1ebbaa099b
Tristan Boulanger <contact@tristanboulanger.fr>
2017-11-16 23:30:45 +0100

    responsive

--- dev/js/main.js
+++ dev/js/main.js
@@ -1,1 +1,18 @@
-const lol = 'maman';
+
+const header = document.querySelector('.bg')
+const navbar = document.getElementById('js-navbar')
+window.onscroll = () => {
+	let windowScrollTop = window.scrollY
+	if(windowScrollTop > 20) {
+		navbar.classList.add('active')
+	} else {
+		navbar.classList.remove('active')
+	}
+
+	let headerRect = header.getBoundingClientRect()
+	let x = headerRect.top
+	let h = headerRect.height
+    console.log('x   '+x)
+    console.log('h   '+h)
+    header.style.backgroundPosition = ('50% '+parseInt(-x / 5)+'px');
+}
