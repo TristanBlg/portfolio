ae4e1802de3cfb9c7e8d90b4e85a173e848019d7
Tristan Boulanger <contact@tristanboulanger.fr>
2017-11-18 19:49:03 +0100

    maj

--- dev/js/sortable.js
+++ dev/js/sortable.js
@@ -11,17 +11,34 @@ class Sortable {
 		let parentWidth = parent.offsetWidth
 		let rectWidth = (parentWidth - (margin * (column - 1))) / column
 		let positionX = 0
+		let arrayRectHeight = []
 
-		this.item.forEach(function(el, i){
-			console.log(i)
-			el.style.position = "absolute"
-			el.style.width = rectWidth+'px'
-			el.style.transform = `translate3d(${positionX}px, 0px, 0px)`
-			if(positionX >= rectWidth * (column - 1)) {
-				positionX = 0
-			} else {
-				positionX = positionX + rectWidth + margin
-			}
+		let test = new Promise(function(resolve, reject){
+			resolve(
+				item.forEach(function(el, i){
+					el.style.position = "absolute"
+					el.style.width = rectWidth+'px'
+					arrayRectHeight.push(el.offsetHeight)
+					console.log(arrayRectHeight)
+					if(i >= column){
+						let rectHeight = arrayRectHeight[i - column] + margin
+						el.style.transform = `translate3d(${positionX}px, ${rectHeight}px, 0px)`
+					} else {
+						el.style.transform = `translate3d(${positionX}px, 0px, 0px)`
+					}
+					if(positionX >= rectWidth * (column - 1)) {
+						positionX = 0
+					} else {
+						positionX = positionX + rectWidth + margin
+					}
+				})
+			)
+		})
+		test.then(function(){
+			let parentHeight = ((Math.floor(item.length / column) * arrayRectHeight[0]) + (margin * (Math.floor(item.length / column) - 1)))
+			parent.style.height = parentHeight+'px'
+		}).catch(function(){
+			console.log('error')
 		})
 	}
 }
