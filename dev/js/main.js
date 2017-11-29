d1fab7e3c548b1532f56d097de16787bfc070063
Tristan Boulanger <contact@tristanboulanger.fr>
2017-11-29 22:30:44 +0100

    Icon <use>

--- dev/js/main.js
+++ dev/js/main.js
@@ -9,13 +9,30 @@ navbarActive = () => {
 		navbar.classList.remove('active')
 	}
 }
-headerParallax = () =>{
+headerParallax = () => {
 	let headerRect = header.getBoundingClientRect()
 	let x = headerRect.top
 	let h = headerRect.height
     header.style.backgroundPositionY = (parseInt(-x / 5)+'px')
 }
+navAutoScroll = () => {
+	let navLink = document.getElementsByClassName('js-navlink')
+	Array.prototype.forEach.call(navLink, el => {
+		el.addEventListener('click', function(ev) {
+			ev.preventDefault()
+			let hrefVal = this.getAttribute('href')
+			hrefVal = hrefVal.replace( /^#/g , '')
+			if(hrefVal === "home") {
+				// Moove top of doc
+			} else {
+				let target = document.getElementById(hrefVal)
+				// Moove top of element
+			}
+		})
+	})
+}
 
+navAutoScroll()
 headerParallax()
 navbarActive()
 
