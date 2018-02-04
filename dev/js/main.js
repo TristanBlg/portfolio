55717cd731fd977e4058adeb8fc9e58305cfeecf
Tristan Boulanger <contact@tristanboulanger.fr>
2018-02-04 17:29:01 +0100

    [Inte]

--- dev/js/main.js
+++ dev/js/main.js
@@ -1,133 +1,133 @@
 function ready(fn) {
-	if (document.attachEvent ? document.readyState === "complete" : document.readyState !== "loading"){
-		fn();
-	} else {
-		document.addEventListener('DOMContentLoaded', fn);
-	}
+    if (document.attachEvent ? document.readyState === "complete" : document.readyState !== "loading"){
+        fn();
+    } else {
+        document.addEventListener('DOMContentLoaded', fn);
+    }
 }
 function navbarActive(navbar) {
-	if(window.scrollY > 20) {
-		navbar.classList.add('active')
-	} else {
-		navbar.classList.remove('active')
-	}
+    if(window.scrollY > 20) {
+        navbar.classList.add('active')
+    } else {
+        navbar.classList.remove('active')
+    }
 }
 function headerParallax(header) {
-	const headerRect 					= header.getBoundingClientRect()
-	const headerX 						= headerRect.top
-    header.style.backgroundPositionY 	= parseInt(-headerX / 5)+'px'
+    const headerRect                    = header.getBoundingClientRect()
+    const headerX                       = headerRect.top
+    header.style.backgroundPositionY    = parseInt(-headerX / 5)+'px'
 }
 function navAutoScroll() {
-	scrollIt = (destination, duration = 200, callback) => {
-		const start 					= window.pageYOffset
-		const startTime 				= 'now' in window.performance ? performance.now() : new Date().getTime()
-		const documentHeight 			= Math.max(document.body.scrollHeight, document.body.offsetHeight, document.documentElement.clientHeight, document.documentElement.scrollHeight, document.documentElement.offsetHeight)
-		const windowHeight 				= window.innerHeight || document.documentElement.clientHeight || document.getElementsByTagName('body')[0].clientHeight
-		const destinationOffset 		= typeof destination === 'number' ? destination : destination.offsetTop
-		const destinationOffsetToScroll = Math.round(documentHeight - destinationOffset < windowHeight ? documentHeight - windowHeight : destinationOffset)
+    scrollIt = (destination, duration = 200, callback) => {
+        const start                     = window.pageYOffset
+        const startTime                 = 'now' in window.performance ? performance.now() : new Date().getTime()
+        const documentHeight            = Math.max(document.body.scrollHeight, document.body.offsetHeight, document.documentElement.clientHeight, document.documentElement.scrollHeight, document.documentElement.offsetHeight)
+        const windowHeight              = window.innerHeight || document.documentElement.clientHeight || document.getElementsByTagName('body')[0].clientHeight
+        const destinationOffset         = typeof destination === 'number' ? destination : destination.offsetTop
+        const destinationOffsetToScroll = Math.round(documentHeight - destinationOffset < windowHeight ? documentHeight - windowHeight : destinationOffset)
 
-		if('requestAnimationFrame' in window === false) {
-			window.scroll(0, destinationOffsetToScroll)
-			if(callback) {
-				callback()
-			}
-			return
-		}
+        if('requestAnimationFrame' in window === false) {
+            window.scroll(0, destinationOffsetToScroll)
+            if(callback) {
+                callback()
+            }
+            return
+        }
 
-		function scroll() {
-			const now 	= 'now' in window.performance ? performance.now() : new Date().getTime()
-			const time 	= Math.min(1, ((now - startTime) / duration))
+        function scroll() {
+            const now   = 'now' in window.performance ? performance.now() : new Date().getTime()
+            const time  = Math.min(1, ((now - startTime) / duration))
 
-			window.scroll(0, Math.ceil((time * (destinationOffsetToScroll - start)) + start))
+            window.scroll(0, Math.ceil((time * (destinationOffsetToScroll - start)) + start))
 
-			if(window.pageYOffset === destinationOffsetToScroll) {
-				if(callback) {
-					callback()
-				}
-				return
-			}
+            if(window.pageYOffset === destinationOffsetToScroll) {
+                if(callback) {
+                    callback()
+                }
+                return
+            }
 
-			requestAnimationFrame(scroll)
-		}
+            requestAnimationFrame(scroll)
+        }
 
-		scroll()
-	}
+        scroll()
+    }
 
-	const navLink = document.querySelectorAll('[href^="#"]')
+    const navLink = document.querySelectorAll('[href^="#"]')
 
-	Array.prototype.forEach.call(navLink, el => {
-		el.addEventListener('click', function(ev) {
-			ev.preventDefault()
+    Array.prototype.forEach.call(navLink, el => {
+        el.addEventListener('click', function(ev) {
+            ev.preventDefault()
 
-			let hrefVal 	= this.getAttribute('href')
-			hrefVal 		= hrefVal.replace( /^#/g , '')
-			let target 		= document.getElementById(hrefVal)
-			let targetTop 	= target.getBoundingClientRect().top
+            let hrefVal     = this.getAttribute('href')
+            hrefVal         = hrefVal.replace( /^#/g , '')
+            let target      = document.getElementById(hrefVal)
+            let targetTop   = target.getBoundingClientRect().top
 
-			scrollIt(
-				target,
-				200
-			);
-		})
-	})
+            scrollIt(
+                target,
+                200
+            );
+        })
+    })
 }
 function animateAtScroll(animationName = 'fadeIn', windowHeightRatio = 1.2, selectorDataName = 'delay') {
-	const elements = Array.from(document.querySelectorAll(`[data-${selectorDataName}]`));
+    const elements = Array.from(document.querySelectorAll(`[data-${selectorDataName}]`));
 
-	elements.forEach(function(el){
-		switch (animationName) {
-			case 'fadeIn':
-				el.style.opacity = 0;
-				break;
-			case 'fadeOut':
-				el.style.opacity = 1;
-				break;
-			default:
-				console.error('animateAtScroll() -> Animation name is required.')
-		}
-	})
+    elements.forEach(function(el){
+        switch (animationName) {
+            case 'fadeIn':
+                el.style.opacity = 0;
+                break;
+            case 'fadeOut':
+                el.style.opacity = 1;
+                break;
+            default:
+                console.error('animateAtScroll() -> Animation name is required.')
+        }
+    })
 
-	const classStopAnimation = 'animateAtScroll'
-	let windowHeight = window.innerHeight / windowHeightRatio
+    const classStopAnimation = 'animateAtScroll'
+    let windowHeight = window.innerHeight / windowHeightRatio
 
-	function setClass(){
-		elements.forEach(function(el){
-			let sectionTop = el.getBoundingClientRect().top;
-			if(windowHeight > sectionTop) {
-				if(!el.classList.contains(classStopAnimation)) {
-					let delay = el.getAttribute(`data-${selectorDataName}`);
-					el.classList.add(classStopAnimation)
-					setTimeout(function(){
-						el.classList.add(animationName)
-					}, delay)
-				}
-			}
-		})
-	}
-	
-	document.addEventListener('resize', function(){
-		windowHeight = window.innerHeight / windowHeightRatio
-	})
+    function setClass(){
+        elements.forEach(function(el){
+            let sectionTop = el.getBoundingClientRect().top;
+            if(windowHeight > sectionTop) {
+                if(!el.classList.contains(classStopAnimation)) {
+                    let delay = el.getAttribute(`data-${selectorDataName}`);
+                    el.classList.add(classStopAnimation)
+                    setTimeout(function(){
+                        el.classList.add(animationName)
+                    }, delay)
+                }
+            }
+        })
+    }
+    
+    document.addEventListener('resize', function(){
+        windowHeight = window.innerHeight / windowHeightRatio
+    })
 
-	setClass();
-	document.addEventListener('scroll', setClass)
+    setClass();
+    document.addEventListener('scroll', setClass)
 }
 
 ready(function() {
-	const header 		= document.getElementById('js-header')
-	const navbar 		= document.getElementById('js-navbar-desktop')
-	const container 	= 1170;
-	const containerM 	= 980;
-	const containerS 	= 768;
-	const containerXs 	= 480;
+    const header        = document.getElementById('js-header')
+    const navbar        = document.getElementById('js-navbar-desktop')
+    const container     = 1170;
+    const containerM    = 980;
+    const containerS    = 768;
+    const containerXs   = 480;
 
-	navAutoScroll()
-	headerParallax(header)
-	navbarActive(navbar)
-	animateAtScroll()
+    navAutoScroll()
+    headerParallax(header)
+    navbarActive(navbar)
+    animateAtScroll()
 
-	window.addEventListener('scroll', () => {
-		headerParallax(header)
-		navbarActive(navbar)
-	})
+    window.addEventListener('scroll', () => {
+        headerParallax(header)
+        navbarActive(navbar)
+    })
 })
--- dev/js/sortable.js
+++ dev/js/sortable.js
@@ -1,136 +1,136 @@
 class Sortable {
-	constructor({
-		parent 	= document.querySelector('.sortable'),
-		links 	= document.querySelectorAll('[data-sortable-link]'),
-		active 	= 'active',
-		column 	= 3, 
-		margin 	= 20
-	}) {
-		this.parent 	= parent
-		this.links 		= Array.from(links)
-		this.active 	= active
-		this.items 		= Array.from(this.parent.children)
-		this.column 	= column
-		this.margin 	= margin
+    constructor({
+        parent  = document.querySelector('.sortable'),
+        links   = document.querySelectorAll('[data-sortable-link]'),
+        active  = 'active',
+        column  = 3, 
+        margin  = 20
+    }) {
+        this.parent     = parent
+        this.links      = Array.from(links)
+        this.active     = active
+        this.items      = Array.from(this.parent.children)
+        this.column     = column
+        this.margin     = margin
 
-		this.init()
-		this.resize()
-	}
+        this.init()
+        this.resize()
+    }
 
-	orderItems(){
-		let {parent, items, column, margin} = this
-		let windowWidth 					= window.innerWidth
+    orderItems(){
+        let {parent, items, column, margin} = this
+        let windowWidth                     = window.innerWidth
 
-		if(windowWidth <= 980 && windowWidth > 480) {
-			column = 2
-		} else if (windowWidth <= 480) {
-			column = 1
-		}
+        if(windowWidth <= 980 && windowWidth > 480) {
+            column = 2
+        } else if (windowWidth <= 480) {
+            column = 1
+        }
 
-		let parentWidth 	= parent.offsetWidth
-		let rectWidth 		= (parentWidth - (margin * (column - 1))) / column
-		let positionX 		= 0
-		let arrayRectHeight = []
+        let parentWidth     = parent.offsetWidth
+        let rectWidth       = (parentWidth - (margin * (column - 1))) / column
+        let positionX       = 0
+        let arrayRectHeight = []
 
-		new Promise((resolve, reject) => {
-			resolve(
-				items.forEach((el, i) => {
-					el.style.position 	= "absolute"
-					el.style.width 		= rectWidth+'px'
-					arrayRectHeight.push(el.offsetHeight)
-					if(i >= column){
-						let rectHeight 		= (arrayRectHeight[i - column] * Math.floor((i / column))) + (margin * Math.floor((i / column)))
-						el.style.transform 	= `translate3d(${positionX}px, ${rectHeight}px, 0px)`
-					} else {
-						el.style.transform 	= `translate3d(${positionX}px, 0px, 0px)`
-					}
-					if(positionX >= rectWidth * (column - 1)) {
-						positionX = 0
-					} else {
-						positionX = positionX + rectWidth + margin
-					}
-				})
-			)
-		}).then(() => {
-			parent.style.position 	= 'relative'
-			let parentHeight 		= ((Math.ceil(items.length / column) * arrayRectHeight[0]) + (margin * (Math.ceil(items.length / column) - 1)))
-			parent.style.height 	= parentHeight+'px'
-		}).then(() => {
-			// Bug last item get transition before position - Remove setTimeout
-			setTimeout(() => {
-				items.forEach((el, i) => {
-					el.style.transition = 'transform .4s ease-in-out, opacity .2s ease-in-out'
-				})
-			}, 100)
-		}).catch(err => {
-			console.error(err)
-		})
-	}
+        new Promise((resolve, reject) => {
+            resolve(
+                items.forEach((el, i) => {
+                    el.style.position   = "absolute"
+                    el.style.width      = rectWidth+'px'
+                    arrayRectHeight.push(el.offsetHeight)
+                    if(i >= column){
+                        let rectHeight      = (arrayRectHeight[i - column] * Math.floor((i / column))) + (margin * Math.floor((i / column)))
+                        el.style.transform  = `translate3d(${positionX}px, ${rectHeight}px, 0px)`
+                    } else {
+                        el.style.transform  = `translate3d(${positionX}px, 0px, 0px)`
+                    }
+                    if(positionX >= rectWidth * (column - 1)) {
+                        positionX = 0
+                    } else {
+                        positionX = positionX + rectWidth + margin
+                    }
+                })
+            )
+        }).then(() => {
+            parent.style.position   = 'relative'
+            let parentHeight        = ((Math.ceil(items.length / column) * arrayRectHeight[0]) + (margin * (Math.ceil(items.length / column) - 1)))
+            parent.style.height     = parentHeight+'px'
+        }).then(() => {
+            // Bug last item get transition before position - Remove setTimeout
+            setTimeout(() => {
+                items.forEach((el, i) => {
+                    el.style.transition = 'transform .4s ease-in-out, opacity .2s ease-in-out'
+                })
+            }, 100)
+        }).catch(err => {
+            console.error(err)
+        })
+    }
 
-	clickFilter(event, element) {
-		event.preventDefault()
-		let {links, active} = this
-		const dataLink = element.dataset.sortableLink
-		links.forEach(el => {
-			el.classList.remove(active)
-		})
-		element.classList.add(active)
-		let obj = document.querySelectorAll(`[data-sortable]`)
-		let itemsFilter = []
-		new Promise((resolve, reject) => {
-			resolve(
-				Object.keys(obj).map(key => obj[key]).filter(el => {
-					if(dataLink === 'all') {
-						el.style.opacity = '1'
-						itemsFilter.push(el)
-					} else {
-						if(el.dataset.sortable !== dataLink) {
-							el.style.opacity = '0'
-						} else {
-							el.style.opacity = '1'
-							itemsFilter.push(el)
-						}
-					}
-					
-				})
-			)
-		}).then(() => {
-			this.items = itemsFilter
-		}).then(() => {
-			this.orderItems()
-		}).catch(err => {
-			console.error(err)
-		})
-	}
+    clickFilter(event, element) {
+        event.preventDefault()
+        let {links, active} = this
+        const dataLink = element.dataset.sortableLink
+        links.forEach(el => {
+            el.classList.remove(active)
+        })
+        element.classList.add(active)
+        let obj = document.querySelectorAll(`[data-sortable]`)
+        let itemsFilter = []
+        new Promise((resolve, reject) => {
+            resolve(
+                Object.keys(obj).map(key => obj[key]).filter(el => {
+                    if(dataLink === 'all') {
+                        el.style.opacity = '1'
+                        itemsFilter.push(el)
+                    } else {
+                        if(el.dataset.sortable !== dataLink) {
+                            el.style.opacity = '0'
+                        } else {
+                            el.style.opacity = '1'
+                            itemsFilter.push(el)
+                        }
+                    }
+                    
+                })
+            )
+        }).then(() => {
+            this.items = itemsFilter
+        }).then(() => {
+            this.orderItems()
+        }).catch(err => {
+            console.error(err)
+        })
+    }
 
-	linksTriggered(){
-		let {links, clickFilter} = this
-		links.forEach((el, id) => {
-			if(id === 0){
-				el.classList.add('active')
-			}
-			el.style.cursor = "pointer"
-			el.addEventListener('click', event => this.clickFilter(event, el))
-		})
-	}
+    linksTriggered(){
+        let {links, clickFilter} = this
+        links.forEach((el, id) => {
+            if(id === 0){
+                el.classList.add('active')
+            }
+            el.style.cursor = "pointer"
+            el.addEventListener('click', event => this.clickFilter(event, el))
+        })
+    }
 
-	init(){
-		this.linksTriggered()
-		this.orderItems()
-	}
+    init(){
+        this.linksTriggered()
+        this.orderItems()
+    }
 
-	resize() {
-		window.addEventListener('resize', () => {
-			clearTimeout(window.sortableResize)
-			window.sortableResize = setTimeout(() => {
-				this.orderItems()
-			}, 500)
-		})
-	}
+    resize() {
+        window.addEventListener('resize', () => {
+            clearTimeout(window.sortableResize)
+            window.sortableResize = setTimeout(() => {
+                this.orderItems()
+            }, 500)
+        })
+    }
 }
 
 const sortable = new Sortable({
-	parent: document.querySelector('.portfolio-project')
+    parent: document.querySelector('.portfolio-project')
 })
 
 
